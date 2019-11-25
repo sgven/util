b09ac41ff4114d4aa328f62da2384fe023652cab
sgven <xj930312>
2019-11-25 09:32:32 +0800

    first commit

---
+++ src/test/java/demo/java/util/UtilApplicationTests.java
@@ -0,0 +1,14 @@
+package demo.java.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
